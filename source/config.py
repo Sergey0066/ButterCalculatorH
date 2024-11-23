efe41df9355b7a229a61af60b8a626e3e0686c35
Butter <flycombox@gmail.com>
2024-11-23 23:47:20 +0500

    Delete source directory

--- source/config.py
+++
@@ -1,8 +0,0 @@
-# Настройки времени между вводами
-DELAY_BETWEEN_EXPRESSIONS = 2
-
-# Звуковые эффекты (можно добавить свои)
-SOUND_EFFECTS = ["beep1.wav", "beep2.wav", "laugh.mp3"]
-
-# Вероятность срабатывания "троллинга"
-TROLL_PROBABILITY = 0.3
--- source/logger.py
+++
@@ -1,6 +0,0 @@
-from datetime import datetime
-
-def log_expression(expression):
-    """Логирует введенные выражения в файл."""
-    with open("calculator_troll.log", "a") as log_file:
-        log_file.write(f"[{datetime.now()}] Введено: {expression}\n")
